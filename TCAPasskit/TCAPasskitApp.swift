2426cfe44b46eb10762c120fbed372ce57acd492
MoocowTheGodly <austina9996@yahoo.com>
2023-10-26 22:35:39 -0500

    Initial Commit

---
+++ TCAPasskit/TCAPasskitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAPasskitApp.swift
+//  TCAPasskit
+//
+//  Created by Austin Anaya on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct TCAPasskitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
